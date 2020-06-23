3f41e5d44f81ac7e2c8ea436b2901d634869d4f1
MariaPascanu <mc.pascanu@gmail.com>
2020-06-23 17:39:34 +0300

    test github

---
+++ lesson_6/maria_p/internet_coverage.py
@@ -0,0 +1,29 @@
+countries = {
+    'Romania': [
+        {
+            'year': '2019',
+            'coverage': 84
+        }, {
+            'year': '2018',
+            'coverage': 67
+        },
+        {
+            'year': '2011',
+            'coverage': 72
+        }
+    ],
+    'Germany': [
+        {
+            'year': '2019',
+            'coverage': 94
+        }, {
+            'year': '2018',
+            'coverage': 87
+        },
+        {
+            'year': '2011',
+            'coverage': 82
+        }
+    ]
+}
+}
