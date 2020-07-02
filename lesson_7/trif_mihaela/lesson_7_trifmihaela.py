1ca73eedb4d90a174124ec5d0105ef19203e55c7
Mihaela Trif <trifmihaela_88@yahoo.com>
2020-07-02 22:47:23 +0300

    second commit

--- lesson_7/trif_mihaela/lesson_7_trifmihaela.py
+++ lesson_7/trif_mihaela/lesson_7_trifmihaela.py
@@ -12,7 +12,44 @@ def greet(name):
 
 print(greet('World'))
 
-# 2. Given the following function:
-# def divide(first_number, second_number):
-#      return first_number / second_number
-# Create a decorator called safe_divide that will output a message if the division cannot be performed, othervise it will return the result.
+# 2. # Create a decorator called safe_divide that will output a message if the division cannot be performed, othervise it will return the result.
+def decorator_safe_divide(my_function):
+
+    def inner_safe_divide(*args):
+        if my_function(*args) is None:
+            print('The divizion cannot be preformed')
+        return my_function(*args)
+    return inner_safe_divide
+
+@decorator_safe_divide
+def divide(first_number, second_number):
+     return first_number / second_number
+
+print(divide(12,15))
+# 3.Create a decorator called register that will update a list called print_registry with all the decorated functions names.
+def register(my_function):
+    
+    def inner_registry(*args):
+        my_function_name = my_function.__name__
+        return my_function_name
+
+    return inner_registry
+
+print_registry = []
+@register
+def greet(name):
+    return "Greetings {}!".format(name)
+print_registry.append(greet())
+
+def say_hello(name):
+    return "Hello {}!".format(name)
+
+@register
+def say_goodbye(name):
+    return "Goodbye {}!".format(name)
+print_registry.append(say_goodbye())
+
+print(print_registry)
+
+
+
