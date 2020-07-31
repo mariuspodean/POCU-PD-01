f7ee9fd2ff553f3e7d7a6e229549f945e31d77d1
Szilard Vig <vigszilard@gmail.com>
2020-07-31 03:09:00 +0300

    added admin personalizations

--- final_project/szilard_vig/realestate_project/realtors/admin.py
+++ final_project/szilard_vig/realestate_project/realtors/admin.py
@@ -2,4 +2,11 @@ from django.contrib import admin
 
 from .models import Realtor
 
-admin.site.register(Realtor)
+class RealtorAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'employee_of_month', 'email', 'hire_date')
+    list_display_links = ('id', 'name')
+    search_fields = ('name',)
+    list_editable = ('employee_of_month',)
+    list_per_page = 10
+
+admin.site.register(Realtor, RealtorAdmin)
