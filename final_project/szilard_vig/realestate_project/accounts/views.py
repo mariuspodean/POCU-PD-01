a3a30ca00fa54f3f8eacf738ea523980541e23ae
Szilard Vig <vigszilard@gmail.com>
2020-08-02 17:09:02 +0300

    added logout functionality

--- final_project/szilard_vig/realestate_project/accounts/views.py
+++ final_project/szilard_vig/realestate_project/accounts/views.py
@@ -21,9 +21,12 @@ def register(request):
                     return redirect('register')
                 else:
                     user = User.objects.create_user(username=username, password=password, email=email, first_name=first_name, last_name=last_name)
-                    auth.login(request, user)
-                    messages.success(request, 'You are now logged in')
-                    return redirect('index')
+                    # auth.login(request, user)
+                    # messages.success(request, 'You are now logged in')
+                    # return redirect('index')
+                    user.save()
+                    messages.success(request, 'You are now registered')
+                    return redirect('login')
         else:
             messages.error(request, 'Please match passwords')
             return redirect('register')
@@ -46,7 +49,10 @@ def login(request):
         return render(request, 'accounts/login.html')
 
 def logout(request):
-    return redirect('index')
+    if request.method == 'POST':
+        auth.logout(request)
+        messages.success(request, 'You are now logged out')
+        return redirect('index')
 
 def dashboard(request):
     return render(request, 'accounts/dashboard.html')
