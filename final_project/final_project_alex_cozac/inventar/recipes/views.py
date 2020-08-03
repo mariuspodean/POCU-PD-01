7a3148f2cd52236d0b5c0b4f0f5be3d545f48e51
cozac <cozacalex86@gmail.com>
2020-08-04 00:05:10 +0300

    'addedCreateRecipe'

--- final_project/final_project_alex_cozac/inventar/recipes/views.py
+++ final_project/final_project_alex_cozac/inventar/recipes/views.py
@@ -15,24 +15,32 @@ def create_recipe(request):
 
     if request.method == 'POST':
         name = request.POST['name']
+
         item_title = request.POST['item_1']
         item_1 = Item.objects.filter(title=item_title).first()
         item_title = request.POST['item_2']
         item_2 = Item.objects.filter(title=item_title).first()
-        item_3 = request.POST['item_3']
-        item_4 = request.POST['item_4']
-        item_5 = request.POST['item_5']
-        item_6 = request.POST['item_6']
-        item_7 = request.POST['item_7']
-        item_8 = request.POST['item_8']
+        item_title = request.POST['item_3']
+        item_3 = Item.objects.filter(title=item_title).first()
+        item_title = request.POST['item_4']
+        item_4 = Item.objects.filter(title=item_title).first()
+        item_title = request.POST['item_5']
+        item_5 = Item.objects.filter(title=item_title).first()
+        item_title = request.POST['item_6']
+        item_6 = Item.objects.filter(title=item_title).first()
+        item_title = request.POST['item_7']
+        item_7 = Item.objects.filter(title=item_title).first()
+        item_title = request.POST['item_8']
+        item_8 = Item.objects.filter(title=item_title).first()
+
         image =  request.FILES
 
         recipe = Recipe(name=name, item1=item_1, item_2=item_2, item_3=item_3, item_4=item_4, item_5=item_5, item_6=item_6, item_7=item_7, item_8=item_8, image=image)
-
         recipe.save()
+
         return redirect('all_recipes')
     else:
-        return render(request, 'recipes/all_recipes.html', {'items':items})
+        return render(request, 'recipes/create_recipe.html', {'items':items})
 
 # def create_item(request):
 #     distributors = Distributor.objects.all
